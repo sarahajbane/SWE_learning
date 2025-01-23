9304792a8870eccb45cdb5ae0a43090767c06475
Sara Hajbane <sara.hajbane@gmail.com>
2025-01-23 21:33:54 +0100

    start

--- lib_create_practice/masterlib.py
+++ lib_create_practice/masterlib.py
@@ -4,6 +4,16 @@ def sum_of_digits(num):
       sum += int(digit)
     return sum
 
+def sum_to_single_digit(num):
+    sum = 0
+    for digit in str(num):
+        sum += int(digit)
+    while 0 <= sum <= 9:
+        for digit in str(sum):
+            sum += int(digit)
+    return sum
+
+
 def main():
     print("Welcome to masterlib library! Enjoy your usage!")
 
---
+++ lib_create_practice/test.py
@@ -0,0 +1,5 @@
+import masterlib as ml
+
+print(ml.sum_of_digits(1925))
+print(ml.sum_to_single_digit(1925))
+
