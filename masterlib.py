eefb7a7bdc4a34d7ffdbc11c26131c7474d1dbe8
Sara Hajbane <sara.hajbane@gmail.com>
2025-01-23 20:47:21 +0100

    creating python libraries start

---
+++ masterlib.py
@@ -0,0 +1,10 @@
+def sum_of_digits(num):
+    sum = 0
+    for digit in str(num):
+      sum += int(digit)
+    return sum
+
+def main():
+    print("Welcome to masterlib library! Enjoy your usage!")
+
+main()
