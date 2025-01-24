b981ad56acee53f753679eb802dd09ec68151ebd
Sara Hajbane <sara.hajbane@gmail.com>
2025-01-24 16:57:35 +0100

    numerology start

---
+++ lib_create_practice/magicnumber.py
@@ -0,0 +1,14 @@
+def lifepath(num):
+    sum = 0
+    while num > 0 or sum > 9:
+        if num == 0:
+            num = sum
+            sum = 0
+        sum += num % 10
+        num //= 10
+    return sum
+
+
+def main():
+    print("Welcome to magicnumber! Calculate your personal numerology here!")
+main()
